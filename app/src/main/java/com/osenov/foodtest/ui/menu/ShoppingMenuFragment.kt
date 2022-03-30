bac0a39191f9e7cba40ab2be7fdccfef9b4eac2d
Sergey Osyonov <BrainScreame@gmail.com>
2022-03-30 17:00:33 +0300

    added colapsing toolbar

--- app/src/main/java/com/osenov/foodtest/ui/menu/ShoppingMenuFragment.kt
+++ app/src/main/java/com/osenov/foodtest/ui/menu/ShoppingMenuFragment.kt
@@ -1,10 +1,40 @@
 package com.osenov.foodtest.ui.menu
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.osenov.foodtest.R
+import com.google.android.material.tabs.TabLayout
+import com.osenov.foodtest.databinding.FragmentShoppingMenuBinding
 
 
-class ShoppingMenuFragment : Fragment(R.layout.fragment_shopping_menu) {
+class ShoppingMenuFragment : Fragment() {
+
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        FragmentShoppingMenuBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imageQRCode.setOnClickListener{
+            Toast.makeText(requireContext(), "Click", Toast.LENGTH_LONG).show()
+        }
+
+        binding.tabLayoutCategory.addTab(binding.tabLayoutCategory.newTab().setText("Пицца"))
+        binding.tabLayoutCategory.addTab(binding.tabLayoutCategory.newTab().setText("Комбо"))
+        binding.tabLayoutCategory.addTab(binding.tabLayoutCategory.newTab().setText("Закуски"))
+    }
 
 
 }
